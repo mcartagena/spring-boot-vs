faf9a5cbf56d32a37abc15b21efcc6902d4b0ff3
mcartagena <marcelocartagenaorellana@gmail.com>
2020-10-06 11:20:02 -0300

    Creating a Hello Spring Boot application

---
+++ back-example/src/main/java/com/mcartagena/back/backexample/BackExampleApplication.java
@@ -0,0 +1,15 @@
+package com.mcartagena.back.backexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackExampleApplication.class, args);
+		// empezamos la programacion aqui
+		
+	}
+
+}
---
+++ back-example/src/test/java/com/mcartagena/back/backexample/BackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcartagena.back.backexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
